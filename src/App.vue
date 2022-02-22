a211ebb8aed87eb7faceb40f66810b1bd8c6e1c3
peng-plus-li <hk5241138@163.com>
2022-02-22 21:26:13 +0800

    xx

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App2" />
+    <HelloWorld msg="Welcome to Your Vue.js App3" />
   </div>
 </template>
 
